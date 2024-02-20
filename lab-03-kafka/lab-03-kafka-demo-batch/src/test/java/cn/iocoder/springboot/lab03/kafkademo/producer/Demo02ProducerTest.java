33530118082f63feb11de652005820652eb0058f
yangshuai <540283645@qq.com>
2024-02-20 17:54:13 +0800

    添加注释

--- lab-03-kafka/lab-03-kafka-demo-batch/src/test/java/cn/iocoder/springboot/lab03/kafkademo/producer/Demo02ProducerTest.java
+++ lab-03-kafka/lab-03-kafka-demo-batch/src/test/java/cn/iocoder/springboot/lab03/kafkademo/producer/Demo02ProducerTest.java
@@ -22,6 +22,16 @@ public class Demo02ProducerTest {
     @Autowired
     private Demo02Producer producer;
 
+    /**
+     * Kafka 提供的批量发送消息，它提供了一个 RecordAccumulator 消息收集器，
+     * 将发送给相同 Topic 的相同 Partition 分区的消息们，“偷偷”收集在一起，
+     * 当满足条件时候，一次性批量发送提交给 Kafka Broker 。如下是三个条件，满足任一即会批量发送：
+     *
+     * 【数量】batch-size ：超过收集的消息数量的最大条数。
+     * 【空间】buffer-memory ：超过收集的消息占用的最大内存。
+     * 【时间】linger.ms ：超过收集的时间的最大等待时长，单位：毫秒。
+     * @throws InterruptedException
+     */
     @Test
     public void testASyncSend() throws InterruptedException {
         logger.info("[testASyncSend][开始执行]");
--- lab-03-kafka/lab-03-kafka-demo/src/main/java/cn/iocoder/springboot/lab03/kafkademo/consumer/Demo01Consumer.java
+++ lab-03-kafka/lab-03-kafka-demo/src/main/java/cn/iocoder/springboot/lab03/kafkademo/consumer/Demo01Consumer.java
@@ -11,6 +11,15 @@ public class Demo01Consumer {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * @KafkaListener注解常用的属性有以下几个：
+     * topics：指定订阅的 topic 名称，支持订阅多个 topic。
+     * groupId：指定消费组的 groupId，用于标识消费者。
+     * id：指定消费者的 id，用于标识消费者。
+     * containerFactory：指定 KafkaListenerContainerFactory 的 Bean 名称，用于指定使用的 KafkaListenerContainerFactory。
+     * errorHandler：指定 KafkaListenerErrorHandler 的 Bean 名称，用于指定使用的 KafkaListenerErrorHandler。
+     * @param message
+     */
     @KafkaListener(topics = Demo01Message.TOPIC,
             groupId = "demo01-consumer-group-" + Demo01Message.TOPIC)
     public void onMessage(Demo01Message message) {
